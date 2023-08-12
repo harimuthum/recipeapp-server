c1d3a1b27fec3213b0b18a069b907eefe301ad7a
HARIMUTHU-M <harimuthutce@gmail.com>
2023-08-12 20:46:22 +0530

    env added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-
 import { userRouter } from "./routes/user-routes.js";
 import { recipesRouter } from "./routes/recipe-router.js";
 
@@ -16,14 +15,11 @@ app.use("/api/auth", userRouter);
 app.use("/api/recipes", recipesRouter);
 
 mongoose
-  .connect(
-    "mongodb+srv://harimuthu:hari123@cluster0.r5019kz.mongodb.net/recipes",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .catch((error) => console.log("Atlas not connected"))
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch(() => console.log("Atlas not connected"))
   .then(() => console.log("Connected to Mongo Atlas"));
 
 app.listen(5000, () => console.log("Server is running on port 5000"));
